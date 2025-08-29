48e8a5154217ef12a1adb89f4c324898e0fafef6
yicheng <ylia0127@student.monash.edu>
2025-08-29 20:02:24 +1000

    basic page setting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import JSON from './components/JSON.vue'
+import JSONSolution from './components/JSONSolution.vue'
+import Form from './components/Form.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!-- <JSON /> -->
+  <!-- <JSONSolution /> -->
+  <Form />
 </template>
 
 <style scoped></style>
---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form">
+    <h1>User Information Form / Credentials</h1>
+    <form>
+      <label for="username">Username:</label><br />
+      <input type="text" id="username" name="username" /><br />
+
+      <label for="password">Password:</label><br />
+      <input type="password" id="password" name="password" /><br />
+
+      <label for="isAustralian">Australian Resident?</label><br />
+      <input type="checkbox" id="isAustralian" name="isAustralian" /><br />
+
+      <label for="reason">Reason For Joining:</label><br />
+      <textarea id="reason" name="reason" rows="3"></textarea><br />
+
+      <label for="gender">Gender</label><br />
+      <select id="gender">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="other">Other</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script setup>
+// Our logic will go here
+</script>
+
+<style scoped>
+/* Our logic will go here*/
+</style>
