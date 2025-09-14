26c962fe311a9c5cd14be092597f07eb98f4b3fa
yicheng <ylia0127@student.monash.edu>
2025-09-14 20:59:20 +1000

    npm install

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-//import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDA0vxcYJjwYQRjPxwvnzVFSCL6U47Zfj4',
+  authDomain: 'ycl-badminton-for-health.firebaseapp.com',
+  projectId: 'ycl-badminton-for-health',
+  storageBucket: 'ycl-badminton-for-health.firebasestorage.app',
+  messagingSenderId: '3287198331',
+  appId: '1:3287198331:web:f9d8b5d85cc13cc2b990de',
+}
+
+initializeApp(firebaseConfig)
+
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(router)
+app.mount('#app')
