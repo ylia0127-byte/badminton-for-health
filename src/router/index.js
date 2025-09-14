ea6c1d0dedd6a36481f623c37d2b245658e2929d
yicheng <ylia0127@student.monash.edu>
2025-09-14 21:11:11 +1000

    npm router insatll

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 import FirebaseSigninView from '@/views/FirebaseSigninView.vue'
 import FirebaseRegisterView from '@/views/FirebaseRegisterView.vue'
 import RatingView from '@/views/RatingView.vue'
